f75c53c84192ec43a6a631e70e3083ba9b05bef4
abdullah-NO <abdullah.hussain97@hotmail.com>
2023-02-12 20:31:06 +0100

    feature-added hero fields

---
+++ Hero/Hero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGHeroes.Hero
+{
+    internal class Hero
+    {
+        public string heroName { get; set; }
+        public int level { get; set; }
+        public int levelAttributes { get; set; }
+        public int Equipment { get; set; }
+        public List<int>[] validWeaponTypes { get; set; }
+        public List<int>[] validArmorTypes { get; set; }
+
+    }
+}
---
+++ Item/Weapon/WeaponsSelection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGHeroes.Item.Weapon
+{
+    
+        public enum Weapons
+        {
+            Axe,
+            Bow,
+            Dagger,
+            Hammer,
+            Staff,
+            Sword,
+            Wand
+        }
+}
